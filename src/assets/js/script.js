ce6606067dce31af74fee12687ebb3af60d7f387
JRH-DC261 <t-hori@hotmail.com>
2018-03-07 23:45:48 +0900

    コンポーネント名をMCSS準拠化

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
 
     if (viewportWidth < iPadPro) {
         //1団体表示
-        $(".slide div").removeClass("GroupL GroupM GroupR").addClass("slide");
-        $(".GroupSlide").removeClass("slide");
+        $(".slide div").removeClass("group-info_L group-info_M group-info_R").addClass("slide");
+        $(".group-slide").removeClass("slide");
     }
 
     //画面リサイズ時再読み込み
@@ -31,33 +31,33 @@ $(document).ready(function () {
 
     //ロード時のセクション名を表示
     if (location.hash == "#Home") {
-    $("#header-currentSection-sp").html("ホーム");
+    $("#header_current-section-sp").html("ホーム");
     }else if (location.hash == "#Information") {
-    $("#header-currentSection-sp").html("ご案内");
+    $("#header_current-section-sp").html("ご案内");
     }else if (location.hash == "#Access") {
-    $("#header-currentSection-sp").html("アクセス");
+    $("#header_current-section-sp").html("アクセス");
     }else if (location.hash == "#Group") {
-    $("#header-currentSection-sp").html("参加団体");
+    $("#header_current-section-sp").html("参加団体");
     }else if (location.hash == "#Timetable") {
-    $("#header-currentSection-sp").html("タイムテーブル");
+    $("#header_current-section-sp").html("タイムテーブル");
     }else if (location.hash == "#Downloads") {
-    $("#header-currentSection-sp").html("ダウンロード");
+    $("#header_current-section-sp").html("ダウンロード");
     }
 
     //スクロール先のセクション名に切り替え
     $(window).on('hashchange', function(){
         if (location.hash == "#Home") {
-        $("#header-currentSection-sp").html("ホーム");
+        $("#header_current-section-sp").html("ホーム");
         }else if (location.hash == "#Information") {
-        $("#header-currentSection-sp").html("ご案内");
+        $("#header_current-section-sp").html("ご案内");
         }else if (location.hash == "#Access") {
-        $("#header-currentSection-sp").html("アクセス");
+        $("#header_current-section-sp").html("アクセス");
         }else if (location.hash == "#Group") {
-        $("#header-currentSection-sp").html("参加団体");
+        $("#header_current-section-sp").html("参加団体");
         }else if (location.hash == "#Timetable") {
-        $("#header-currentSection-sp").html("タイムテーブル");
+        $("#header_current-section-sp").html("タイムテーブル");
         }else if (location.hash == "#Downloads") {
-        $("#header-currentSection-sp").html("ダウンロード");
+        $("#header_current-section-sp").html("ダウンロード");
         }
     });
 
@@ -91,28 +91,29 @@ $(document).ready(function () {
     $('.fp-next').append('<span class="fa fa-angle-right" id="arrowR"></span>');
 
     //団体一覧部門Sticky
-    $(".GroupSection").sticky({topSpacing:39});
+    $(".group-section").sticky({topSpacing:39});
 
     //対応する団体情報を開く
     //団体表示
     if (viewportWidth < iPadPro) {
-        $('.GroupExhibit').click(function() {
-            var GroupInfoNumber = $(this).closest('tr').index();
-            var GroupInfoOpenSrc = 'GroupExhibit.html#Home/' + GroupInfoNumber;
-            $('.GroupInfo').attr('src', GroupInfoOpenSrc);
-            $('.GroupInfo').css('visibility','visible').animate({opacity: 1}, 400);
+        $('.group-exhibit').click(function() {
+            var groupInfoNumber = $(this).closest('tr').index();
+            var groupInfoOpenSrc = 'GroupExhibit.html#Home/' + groupInfoNumber;
+            $('.group-info_iframe').attr('src', groupInfoOpenSrc);
+            $('.group-info').css('visibility','visible').animate({opacity: 1}, 400);
         });
     }else{
-        $('.GroupExhibit').click(function() {
-            var GroupInfoNumber = $(this).closest('tr').index();
-            var GroupInfoOpenSrc = 'GroupExhibit.html#Home/' + Math.floor(GroupInfoNumber / 3);
-            $('.GroupInfo').css('visibility','visible').animate({opacity: 1}, 400);
+        $('.group-exhibit').click(function() {
+            var groupInfoNumber = $(this).closest('tr').index();
+            var groupInfoOpenSrc = 'GroupExhibit.html#Home/' + Math.floor(groupInfoNumber / 3);
+            $('.group-info_iframe').attr('src', groupInfoOpenSrc);
+            $('.group-info').css('visibility','visible').animate({opacity: 1}, 400);
         });
     }
 
-    $('.GroupInfoClose').click(function() {
-        $('.GroupInfo').animate({opacity: 0}, 400, function() {
-            $('.GroupInfo').css('visibility','hidden');
+    $('.group-info_close').click(function() {
+        $('.group-info').animate({opacity: 0}, 400, function() {
+            $('.group-info').css('visibility','hidden');
         });
     });
 });
